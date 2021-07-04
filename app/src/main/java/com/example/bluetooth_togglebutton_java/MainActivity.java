823a0539de061802c62261f73710e101b523085c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-04 14:32:49 +0530

    add the permission for the BLUETOOTH inside the AndroidManifest.xml file

--- app/src/main/java/com/example/bluetooth_togglebutton_java/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_togglebutton_java/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.ToggleButton;
 public class MainActivity extends AppCompatActivity {
     ToggleButton mainButton;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
